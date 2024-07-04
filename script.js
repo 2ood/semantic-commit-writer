da35cb25f300450083cb67a56bf39d80651a4c4d
2ood <kyungmin.official0@gmail.com>
2024-07-05 01:48:44 +0900

    feat(clear) : made clear button disable-toggle, made it functional

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ window.onload = () => {
 
 const input_type = document.getElementById("commit_type");
 const input_title = document.getElementById("commit_title");
-const input_issue = document.getElementById("commit_issue");
+
+const advanced_enabled = document.getElementById("advanced_enabled");
+const advanced_breaking = document.getElementById("advanced_breaking");
+const advanced_issue = document.getElementById("advanced_issue");
+const advanced_scope = document.getElementById("advanced_scope");
 
 const button_generate = document.getElementById("generate");
 const button_copy = document.getElementById("copy");
+const button_clear = document.getElementById("clear");
+
+const clearables = document.getElementsByClassName("clearable");
 
 var validationCheck = ()=>{
     if(input_type.value!="unchosen" && input_title.value.length>0) {
@@ -15,7 +22,6 @@ var validationCheck = ()=>{
             button_generate.classList.remove("disabled");
             button_generate.classList.remove("btn-secondary");
             button_generate.classList.add("btn-primary");
-            button_generate.innerHTML="Generate!";
         }
     }
     else {
@@ -23,7 +29,31 @@ var validationCheck = ()=>{
             button_generate.classList.add("disabled");
             button_generate.classList.remove("btn-primary");
             button_generate.classList.add("btn-secondary");
-            button_generate.innerHTML="Fill the blanks to generate";
+        }
+    }
+};
+
+var controlClearButton = () =>{
+    const type_empty = (input_type.value=="unchosen");
+    const title_empty = (input_title.value=="");
+    const desc_empty = (easyMDE.value()=="");
+    const breaking_empty = !(advanced_breaking.checked);
+    const issue_empty = (advanced_issue.value=="");
+    const scope_empty = (advanced_scope.value=="");
+
+    const all_empty = type_empty&&title_empty&&desc_empty&&breaking_empty&&issue_empty&&scope_empty;
+
+    if(!all_empty) {
+        if(button_clear.classList.contains("disabled")){
+            button_clear.classList.remove("disabled");
+            button_clear.classList.remove("btn-outline-danger");
+            button_clear.classList.add("btn-danger");
+        }
+    } else {
+        if(!button_clear.classList.contains("disabled")) {
+            button_clear.classList.add("disabled");
+            button_clear.classList.add("btn-outline-danger");
+            button_clear.classList.remove("btn-danger");
         }
     }
 };
@@ -31,6 +61,10 @@ var validationCheck = ()=>{
 input_type.addEventListener("change",validationCheck);
 input_title.addEventListener("change",validationCheck);
 
+for(var i=0; i<clearables.length;i++) {
+    clearables[i].addEventListener("input",controlClearButton);
+}
+easyMDE.codemirror.on("change", controlClearButton);
 
 button_generate.addEventListener("click",()=>{
     let type = input_type.value;
@@ -44,4 +78,21 @@ button_generate.addEventListener("click",()=>{
 button_copy.addEventListener("click",()=>{
 
 });
+
+button_clear.addEventListener("click",()=>{
+    if(confirm("Do you really want to clear all the inputs?")==true) {
+        input_type.value="unchosen";
+        input_title.value="";
+
+        easyMDE.value("");
+
+        advanced_breaking.checked = false;
+        advanced_issue.value = "";
+        advanced_scope.value = "";
+
+        button_clear.classList.add("disabled");
+        button_clear.classList.add("btn-outline-danger");
+        button_clear.classList.remove("btn-danger");
+    }
+})
 }
