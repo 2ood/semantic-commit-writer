30e3a959dfc0c79862e6bd83e975e49b708ae421
2ood <kyungmin.official0@gmail.com>
2024-07-08 19:05:16 +0900

    docs(script) : added JSDOC

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const easyMDE = new EasyMDE({minWidth : "500px", placeholder: "(Optional) Detail
 
 window.onload = () => {
 
+// locate doms
 const input_type = document.getElementById("commit_type");
 const input_title = document.getElementById("commit_title");
 
@@ -16,6 +17,15 @@ const button_clear = document.getElementById("clear");
 
 const clearables = document.getElementsByClassName("clearable");
 
+/**
+ * @name validationCheck
+ * @function
+ * @description function that checks if required inputs are filled.
+ * This generator's required arguments are : commit type and title.
+ * every time this function is called (by 'onchange' event)
+ * it checks if input_type and input_title is changed and properly filled.
+ * It then enables the generate button only when both input is ready.
+ */
 var validationCheck = ()=>{
     if(input_type.value!="unchosen" && input_title.value.length>0) {
         if(button_generate.classList.contains("disabled")){
@@ -33,6 +43,13 @@ var validationCheck = ()=>{
     }
 };
 
+
+/**
+ * @name controlClearButton
+ * @function
+ * @description This function controls clear button.
+ * The clear button is enabled only when any of the inputs on control is filled.
+ */
 var controlClearButton = () =>{
     const type_empty = (input_type.value=="unchosen");
     const title_empty = (input_title.value=="");
@@ -58,28 +75,39 @@ var controlClearButton = () =>{
     }
 };
 
-input_type.addEventListener("change",validationCheck);
-input_title.addEventListener("input",validationCheck);
+/**
+ * @name generateCommand
+ * @function
+ * @description function for generating the command and showing 
+ * it on the modal.
+ */
 
-for(var i=0; i<clearables.length;i++) {
-    clearables[i].addEventListener("input",controlClearButton);
-}
-easyMDE.codemirror.on("change", controlClearButton);
-
-button_generate.addEventListener("click",()=>{
+var generateCommand = () =>{
     let type = input_type.value;
     let issue = input_issue.value;
     let title = input_title.value;
     let desciption = easyMDE.value();
 
     console.log(type+" "+issue+" "+title+" "+desciption);
-});
+};
 
-button_copy.addEventListener("click",()=>{
 
-});
+/** 
+ * @name copyCommand
+ * @function
+ * @description function for copying the generated command to clipboard.
+ */
+var copyCommand = () =>{
 
-button_clear.addEventListener("click",()=>{
+}
+
+/**
+ * @name clearInputs
+ * @function
+ * @description function for clearing all the inputs into initial state.
+ */
+
+var clearInputs  = () => {
     if(confirm("Do you really want to clear all the inputs?")==true) {
         input_type.value="unchosen";
         input_title.value="";
@@ -94,5 +122,20 @@ button_clear.addEventListener("click",()=>{
         button_clear.classList.add("btn-outline-danger");
         button_clear.classList.remove("btn-danger");
     }
-})
+}
+
+// adding event listeners to input doms.
+input_type.addEventListener("change",validationCheck);
+input_title.addEventListener("input",validationCheck);
+easyMDE.codemirror.on("change", controlClearButton);
+
+//event listeners for clear button enablization.
+for(var i=0; i<clearables.length;i++) {
+    clearables[i].addEventListener("input",controlClearButton);
+}
+
+// linking buttons to its executing functions.
+button_generate.addEventListener("click",generateCommand);
+button_copy.addEventListener("click",copyCommand);
+button_clear.addEventListener("click",clearInputs);
 }
